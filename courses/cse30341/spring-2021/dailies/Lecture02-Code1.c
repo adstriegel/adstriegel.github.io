5a017e6ac1d4dfb7c23a1b9ceadf99c65e5218b0
Aaron Striegel <striegel@nd.edu>
2021-02-08 13:45:44 -0500

    Fix quotes

--- courses/cse30341/spring-2021/dailies/Lecture02-Code1.c
+++ courses/cse30341/spring-2021/dailies/Lecture02-Code1.c
@@ -5,7 +5,7 @@ char bKeepLooping = 1;
 void myFunction (void * pData)
 {
   int * pNumber = (int *) pData;
-  printf(“%d\n”, pNumber[0]);
+  printf("%d\n", pNumber[0]);
 }
 
 int main( int argc, char *argv[] ) 
