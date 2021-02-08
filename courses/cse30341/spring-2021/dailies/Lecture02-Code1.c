f65032c6dd6d05c112955ef0f86e61adf14a1102
Aaron Striegel <striegel@nd.edu>
2021-02-08 13:34:39 -0500

    Add the code for download, add a link to the student machine info for CSE

---
+++ courses/cse30341/spring-2021/dailies/Lecture02-Code1.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+char bKeepLooping = 1; 
+
+void myFunction (void * pData)
+{
+  int * pNumber = (int *) pData;
+  printf(“%d\n”, pNumber[0]);
+}
+
+int main( int argc, char *argv[] ) 
+{ 
+  int nVal = 3;
+  int * pVal;
+
+  pVal = &nVal;
+  myFunction(pVal);  
+}
