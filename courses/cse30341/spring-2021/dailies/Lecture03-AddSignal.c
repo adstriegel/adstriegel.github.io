6d388cb91c87197ee519b9752cca8f23caf749cf
Aaron Striegel <striegel@nd.edu>
2021-02-11 00:15:24 -0500

    Lecture 3 Daily

---
+++ courses/cse30341/spring-2021/dailies/Lecture03-AddSignal.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <signal.h> 
+
+char bKeepLooping = 1; 
+
+int main( int argc, char *argv[] ) 
+{ 
+     while(bKeepLooking) 
+     { } 
+    
+     printf("Exited successfully\n"); 
+     return 0; 
+}
